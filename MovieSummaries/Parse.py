a8781d93aeb814b904c5551a9d9e2dae32068e72
IceCereal <ns_raghav@hotmail.com>
2019-02-24 22:17:39 +0530

    Add more data to important data

--- MovieSummaries/Parse.py
+++ MovieSummaries/Parse.py
@@ -3,36 +3,55 @@ import csv
 
 importantData = []
 genre = []
+language = []
+country = []
 
 allGenres = []
+
+header = ['Wikipedia Movie ID', 'Movie Name', 'Release Date', 'Languages', 'Country of Origin', 'Genre']
 
 with open("movie.metadata.tsv") as Fobj:
 		rd = csv.reader(Fobj, delimiter="\t", quotechar='"')
 		for row in rd:
 			genre = []
+			language = []
+			country = []
+
 			rawDict = row[8]
 			Dict = lit_eval(rawDict)
-
 			for entry in Dict:
 				genre.append(Dict[entry])
+
+			rawDict = row[6]
+			Dict = lit_eval(rawDict)
+			for entry in Dict:
+				language.append(Dict[entry])
+
+			rawDict = row[7]
+			Dict = lit_eval(rawDict)
+			for entry in Dict:
+				country.append(Dict[entry])
 		
 			for entry in genre:
 					if entry not in allGenres:
 						allGenres.append(entry)
 
-			importantData.append([[row[0], row[2], genre]])
+			importantData.append([[row[0], row[2], row[3], language, country, genre]])
 
 
 ### WRITE ALL THE REQUIRED METADATA OF THE MOVIE
 with open("MovieMetadata.csv", 'a') as Fobj:
 	writer = csv.writer(Fobj)
 	
-	for row in importantData:
-		writer.writerow([row])
+	writer.writerow(header)
 
-### WRITE ALL THE MOVIES' GENRES
+	for row in importantData:
+		writer.writerow(row)
+
+"""### WRITE ALL THE MOVIES' GENRES
 with open("AllMovieGenres.csv", 'w') as Fobj:
 	writer = csv.writer(Fobj)
 
 	for genre in allGenres:
 		writer.writerow([genre])
+"""
